0d8e33542a9841c2a2ff5993ea4b74d14dbdac3c
Sam Huynh <samhwang2112.dev@gmail.com>
2021-08-10 19:07:12 +1000

    Remove db setup for backend
    This is premature setup. We should wait until the challenge starts to
    settle on DB choice, because we may not even need a database.

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, make_response
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
 
@@ -9,15 +8,6 @@ load_dotenv()
 app = Flask(__name__)
 
 CORS(app)
-
-# Database configs
-db_user = os.environ["POSTGRES_USER"]
-db_password = os.environ["POSTGRES_PASSWORD"]
-db_name = os.environ["POSTGRES_DB"]
-db_host = os.environ["POSTGRES_HOST"]
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
 
 # Default app routes
 @app.route('/')
--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from api import app, db
+from api import app
 from ariadne import load_schema_from_path, make_executable_schema, graphql_sync, snake_case_fallback_resolvers, ObjectType
 from ariadne.constants import PLAYGROUND_HTML
 from flask import request, jsonify
