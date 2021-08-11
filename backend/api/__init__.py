bea3da269dd004b6ebfbf2fed844060a80a26f69
Sam Huynh <samhwang2112.dev@gmail.com>
2021-08-11 17:55:33 +1000

    Remove default route for backend

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -9,14 +9,6 @@ app = Flask(__name__)
 
 CORS(app)
 
-# Default app routes
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello!'
-
-
 @app.errorhandler(404)
 def not_found(error):
     response = make_response('NOT FOUND', 404)
