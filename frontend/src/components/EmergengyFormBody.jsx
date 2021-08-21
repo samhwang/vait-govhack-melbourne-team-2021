589d0b6bf77899ea1d26cf88e46d155d8fa30d8c
Sam Huynh <samhwang2112.dev@gmail.com>
2021-08-22 09:45:34 +1000

    Add more phone numbers for emergencies

--- frontend/src/components/EmergengyFormBody.jsx
+++ frontend/src/components/EmergengyFormBody.jsx
@@ -12,9 +12,23 @@ export default function EmergencyFormBody({ closeModal }) {
       <Grid item md={12} style={{ margin: '2em' }}>
         <Typography variant="h2">Emergency</Typography>
         <Typography variant="body1">
-          For emergency situations, click{' '}
+          For life-threatening emergencies, click{' '}
           <a href="tel:000" style={{ fontWeight: 'bold' }}>
             here to dial 000
+          </a>
+        </Typography>
+        <Typography variant="body1">
+          For non-urgent crime and events anytime, anywhere 24 hours
+          a day, seven days a week, click
+          {' '}
+          <a href="tel:131444" style={{ fontWeight: 'bold' }}>
+            here to dial 131 444
+          </a>
+        </Typography>
+        <Typography variant="body1">
+          For flood, storm, tsunami and earthquake emergencies, click{' '}
+          <a href="tel:132500" style={{ fontWeight: 'bold' }}>
+            here to dial 132 500
           </a>
         </Typography>
       </Grid>
