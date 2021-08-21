cbdb92aee78da5306e2740af1399f976cec549d7
Sam Huynh <samhwang2112.dev@gmail.com>
2021-08-21 18:55:19 +1000

    Add alert to Map page

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,9 +1,21 @@
+import { useGeolocation } from 'rooks';
+import { Alert } from '@material-ui/lab';
+import { Grid, Typography } from '@material-ui/core';
 import SharedLayout from './SharedLayout';
 
 function Map() {
+  const geolocation = useGeolocation();
+
   return (
     <SharedLayout>
-      <h1>Map Page</h1>
+      {geolocation && geolocation.isError && (
+        <Alert variant="filled" severity="warning">
+          Please enable location services to use this module.
+        </Alert>
+      )}
+      <Grid item container md={12}>
+        <Typography variant="h2">Map Page</Typography>
+      </Grid>
     </SharedLayout>
   );
 }
