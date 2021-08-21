75da21f73a56a69f550227e7321f762bb3ed8c5f
Daniel Nguyen <xuananh.nguyenha@gmail.com>
2021-08-22 04:25:08 +1000

    finally getting to bed

--- backend/api/queries.py
+++ backend/api/queries.py
@@ -10,11 +10,80 @@ from ariadne import ObjectType
 import boto3
 import pandas as pd
 import io
+import random
 
-from utils import add_random_data, s3_get_file
+
+def get_random_features():
+    features = ['Furniture/seating',
+                'Shops/cafe/kiosk',
+                'Market/stall',
+                'Stage/event space',
+                'Community building',
+                'Shade structure',
+                'Playspace',
+                'Sportfield/court',
+                'Toilets/amenities',
+                'Drinking water',
+                'Way-finding signage',
+                'Public or community art',
+                'Educational program/space',
+                'Meeting room/space',
+                'Public transport options',
+                'Welcome desk/information pod',
+                'Heritage elements',
+                'Trees/plants',
+                'Beach/foreshore',
+                'Wetlands/river/water',
+                'Animals/wildlife']
+
+    return random.choices(features, k=random.choice(range(0, len(features))))
+
+
+def get_visitor_count():
+    return random.choice(range(0, 20))
+
+
+def get_rating_by_hour():
+    return [
+        {'hr': '06:00', 'rating': random.choice(range(0, 5))},
+        {'hr': '12:00', 'rating': random.choice(range(0, 5))},
+        {'hr': '18:00', 'rating': random.choice(range(0, 5))},
+    ]
+
+
+def get_rating_by_day():
+    return [
+        {'day': '21/08/2021', 'rating': random.choice(range(0, 5))},
+        {'day': '22/08/2021', 'rating': random.choice(range(0, 5))},
+        {'day': '23/08/2021', 'rating': random.choice(range(0, 5))},
+        {'day': '24/08/2021', 'rating': random.choice(range(0, 5))},
+        {'day': '25/08/2021', 'rating': random.choice(range(0, 5))},
+        {'day': '26/08/2021', 'rating': random.choice(range(0, 5))},
+        {'day': '27/08/2021', 'rating': random.choice(range(0, 5))},
+    ]
+
+
+def add_random_data(obj):
+    for item in obj:
+        item['features'] = get_random_features()
+        item['visitorCount'] = get_visitor_count()
+        item['ratingByHr'] = get_rating_by_hour()
+        item['ratingByDay'] = get_rating_by_day()
+
+
+def s3_get_file():
+    s3_client = boto3.client('s3')
+
+    data_object = s3_client.get_object(
+        Bucket='vaitgovhackmelb', Key='data.csv')
+
+    df = pd.read_csv(io.BytesIO(data_object['Body'].read()))
+
+    return df
+
 
 # Where the fun begins
-data = s3_get_file()
+data = s3_get_file().to_dict('records')
 add_random_data(data)
 
 ###################################
@@ -36,10 +105,18 @@ def resolve_rating_by_day(obj, *_):
 
 
 @query.field("publicSpaces")
-def resolve_public_space(obj, info):
+def resolve_public_space(*_, limit = 10):
+    if limit > 50:
+        payload = {
+            "success": False,
+            "errors": ["Maximum records allowed to query reached."],
+            "public_spaces": [],            
+        }
+        return payload
+
     try:
         # Get data and pass to payload
-        public_spaces = data
+        public_spaces = data[:limit]
         payload = {
             "success": True,
             "public_spaces": public_spaces
@@ -48,5 +125,6 @@ def resolve_public_space(obj, info):
         payload = {
             "success": False,
             "errors": [str(error)],
+            "public_spaces": [],
         }
     return payload
