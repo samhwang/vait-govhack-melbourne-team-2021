bc20fff8547e997e296e7662a1b144c195794110
Sam Huynh <samhwang2112.dev@gmail.com>
2021-08-21 19:06:23 +1000

    Add sample mapbox component

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from 'react';
 import { useGeolocation } from 'rooks';
 import { Alert } from '@material-ui/lab';
 import { Grid, Typography } from '@material-ui/core';
+import ReactMapGL from 'react-map-gl';
 import SharedLayout from './SharedLayout';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN =
+  'pk.eyJ1Ijoic2FtaHdhbmcyMTEyIiwiYSI6ImNrc2xqZXpycDA3d28ydnBuMnFjYWkzdWcifQ.6L8Ejvo0e13O7j5iy01VjQ';
+
+const DEFAULT_GPS_COORDINATES = {
+  latitude: -37.8136,
+  longitude: 144.9631,
+};
 
 function Map() {
   const geolocation = useGeolocation();
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    zoom: 8,
+    ...DEFAULT_GPS_COORDINATES,
+  });
+  const onViewportChange = (nextViewport) => setViewport(nextViewport);
+
+  useEffect(() => {
+    if (!geolocation) {
+      return;
+    }
+
+    setViewport((currentViewport) => ({
+      ...currentViewport,
+      latitude: geolocation.lat,
+      longitude: geolocation.lng,
+    }));
+  }, [geolocation]);
 
   return (
     <SharedLayout>
@@ -15,6 +45,14 @@ function Map() {
       )}
       <Grid item container md={12}>
         <Typography variant="h2">Map Page</Typography>
+
+        <Grid item container md={12}>
+          <ReactMapGL
+            {...viewport}
+            onViewportChange={onViewportChange}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+        </Grid>
       </Grid>
     </SharedLayout>
   );
