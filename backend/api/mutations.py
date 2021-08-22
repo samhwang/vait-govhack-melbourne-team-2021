07761fec08762716a38566b46d898c63838cafa1
Sam Huynh <samhwang2112.dev@gmail.com>
2021-08-22 10:22:52 +1000

    Fix typo in email function

--- backend/api/mutations.py
+++ backend/api/mutations.py
@@ -1,34 +1,25 @@
-import urllib3
-import json
-import os
-import datetime
-import boto3
-import sys
-
-
-from ariadne import ObjectType
-
 try:
     import unzip_requirements
 except ImportError:
     pass
 
 from ariadne import ObjectType
+import urllib3
+import os
+import boto3
 
 mutation = ObjectType("Mutation")
-
-s3_client = boto3.client('s3')
-
-email_template = s3_client.get_object(Bucket='vaitgovhackmelb', Key='index.htm')
-email_template = email_template['Body'].read()
-
-BODY_HTML = email_template
 
 def email_sender(recipient, subject):
     sender = os.environ['sender']
     client = boto3.client('ses')
     CHARSET = "UTF-8"
-    BODY_HTML = f.read()
+
+    s3_client = boto3.client('s3')
+    email_template = s3_client.get_object(Bucket='vaitgovhackmelb', Key='index.htm')
+    email_template = email_template['Body'].read()
+    BODY_HTML = email_template
+
     response = client.send_email(
         Source='samhwang21121994@yahoo.com.vn',
         Destination={
@@ -55,6 +46,6 @@ def Subscribe(obj, info, email, suburb):
     email_sender(recipient=email, subject=subject)
     payload = {
         'success': True,
-        'message': f"Email {email} subscribed successful for suburb {suburb}!"
+        'message': f"Email {email} subscribed successfully for suburb {suburb}!"
     }
     return payload
